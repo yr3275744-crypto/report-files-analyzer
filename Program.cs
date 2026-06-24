cde3305507525f8430aa9559eb269c3db563e0be
Ytshak_Raiyner <yr3275744@gmail.com>
2026-06-24 12:04:54 +0300

    add load file methd

--- Program.cs
+++ Program.cs
@@ -1,1 +1,36 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System;
+using System.IO;
+namespace ReportFilesAnalyzer;
+
+class Manager
+{
+    static string[]? LoadFile(string? aPath)
+    {
+        if (File.Exists(aPath))
+        {
+            string[] reports = File.ReadAllLines(aPath);
+            if (reports.Length > 0)
+            {
+                string[]? nullableReports = reports;
+                return nullableReports;
+            }
+            else
+            {
+                Console.WriteLine("Empty file.");
+                return null;
+            }
+        }
+        else
+        {
+            Console.WriteLine("The file did not exists.");
+            return null;
+        }
+    }
+
+    static void Main()
+    {
+        string path = @".\reports.txt";
+        string[]? reports = LoadFile(path);
+    }
+      
+}
